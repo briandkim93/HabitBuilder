fbe77d0e94c0fbadc3cff4dee3d425ff947c6c6d
Brian Kim <briandkim93@gmail.com>
2018-08-12 15:28:59 -0700

    Add functionality to remove habits from manage template

--- habit/views.py
+++ habit/views.py
@@ -9,36 +9,42 @@ from .models import Habit
 @login_required(login_url='/')
 def manage(request):
     if request.method == 'POST':
-        habit = request.POST['habit']
-        days = request.POST.getlist('days')
-        context = {
-            'habit': habit,
-            'error': ''
-        }
-        if not habit:
-            context['error'] == 'Please enter a habit name'
-            return render(request, 'habit/manage.html', context)
-        if len(days) == 0:
-            context['error'] == 'Please select the days that this habit is to be completed'
-            return render(request, 'habit/manage.html', context)
-        new_habit = Habit()
-        new_habit.habit = habit
-        new_habit.user = request.user
-        new_habit.date_created = timezone.datetime.now()
-        new_habit.save()
-        for day in days:
-            new_day = Day.objects.get(day=day)
-            new_habit.days.add(new_day)
-        new_habit.save()
-        return redirect('manage')
+        if request.POST['submit'] == 'Add Habit':
+            habit = request.POST['habit']
+            days = request.POST.getlist('days')
+            context = {
+                'habit': habit,
+                'error': ''
+            }
+            if not habit:
+                context['error'] == 'Please enter a habit name'
+                return render(request, 'habit/manage.html', context)
+            if len(days) == 0:
+                context['error'] == 'Please select the days that this habit is to be completed'
+                return render(request, 'habit/manage.html', context)
+            new_habit = Habit()
+            new_habit.habit = habit
+            new_habit.user = request.user
+            new_habit.date_created = timezone.datetime.now()
+            new_habit.save()
+            for day in days:
+                new_day = Day.objects.get(day=day)
+                new_habit.days.add(new_day)
+            new_habit.save()
+            return redirect('manage')
+        else:
+            habit_id = request.POST['remove']
+            habit = Habit.objects.get(id=habit_id)
+            habit.delete()
+            return redirect('manage')
     else:
-        habits_and_days = []
+        habit_details = []
         for habit in Habit.objects.all():
             days = []
             for day in habit.days.all():
                 days += [day.day]
-            habits_and_days += [(habit.habit, days)]
+            habit_details += [[habit.id, habit.habit, days]]
         context = {
-            'habits_and_days': habits_and_days,
+            'habit_details': habit_details,
         }
         return render(request, 'habit/manage.html', context)
