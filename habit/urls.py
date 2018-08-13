256c97800eaa540815a17770971369de0b50e2cd
Brian Kim <briandkim93@gmail.com>
2018-08-12 17:20:37 -0700

    Add functionality to populate habits into day template

--- habit/urls.py
+++ habit/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('day/<slug:date_slug>/', views.day, name='day'),
     path('manage/', views.manage, name='manage')
 ]
--- habit/views.py
+++ habit/views.py
@@ -1,11 +1,57 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from datetime import date, timedelta
 
 from .models import Day
 from .models import Habit
 
 # Create your views here.
+@login_required(login_url='/')
+def day(request, date_slug):
+    date_obj = date.fromisoformat(date_slug)
+    day_int = date_obj.weekday()
+    day_str = ''
+    if day_int == 0:
+        day_str = 'Monday'
+        day_str_abbr = 'mon'
+    elif day_int == 1:
+        day_str = 'Tuesday'
+        day_str_abbr = 'tue'
+    elif day_int == 2:
+        day_str = 'Wednesday'
+        day_str_abbr = 'wed'
+    elif day_int == 3:
+        day_str = 'Thursday'
+        day_str_abbr = 'thu'
+    elif day_int == 4:
+        day_str = 'Friday'
+        day_str_abbr = 'fri'
+    elif day_int == 5:
+        day_str = 'Saturday'
+        day_str_abbr = 'sat'
+    elif day_int == 6:
+        day_str = 'Sunday'
+        day_str_abbr = 'sun'
+    prev_date_slug = str(date_obj - timedelta(days=1))
+    next_date_slug = str(date_obj + timedelta(days=1))
+    habit_details = []
+    for habit in Habit.objects.filter(user__username=request.user.username):
+        days = []
+        for day in habit.days.all():
+            days += [day.day]
+        should_display = habit.date_created <= date_obj
+        habit_details += [[habit.id, habit.habit, days, should_display]]
+    context = {
+        'date': date_slug,
+        'day_str': day_str,
+        'day_str_abbr': day_str_abbr,
+        'prev_date_slug': prev_date_slug,
+        'next_date_slug': next_date_slug,
+        'habit_details': habit_details
+    }
+    return render(request, 'habit/day.html', context)
+
 @login_required(login_url='/')
 def manage(request):
     if request.method == 'POST':
@@ -25,7 +71,7 @@ def manage(request):
             new_habit = Habit()
             new_habit.habit = habit
             new_habit.user = request.user
-            new_habit.date_created = timezone.datetime.now()
+            new_habit.date_created = timezone.localdate()
             new_habit.save()
             for day in days:
                 new_day = Day.objects.get(day=day)
@@ -39,7 +85,7 @@ def manage(request):
             return redirect('manage')
     else:
         habit_details = []
-        for habit in Habit.objects.all():
+        for habit in Habit.objects.filter(user__username=request.user.username):
             days = []
             for day in habit.days.all():
                 days += [day.day]
