cfeed6923bd92224dd4e0df956f1dcdfa7959b48
Brian Kim <briandkim93@gmail.com>
2018-08-13 17:08:29 -0700

    Wrap script in conditional that checks the environment

--- habit_builder/static/js/manage.js
+++ habit_builder/static/js/manage.js
@@ -1,16 +1,18 @@
 let add_menu_el = document.getElementsByClassName('add-menu')[0];
 let close_button_el = document.getElementsByClassName('close-button')[0];
-let days_list_el = document.getElementById('days-list');
+let days_list_el = document.getElementsByClassName('days-list')[0];
 
-close_button_el.addEventListener('click', function() {
-    add_menu_el.classList.add('hidden');
-}, false);
+if (add_menu_el && close_button_el && days_list_el) {
+    close_button_el.addEventListener('click', function() {
+        add_menu_el.classList.add('hidden');
+    }, false);
 
-days_list_el.addEventListener('click', function(event) {
-    day_item_el = event.target;
-    if (day_item_el.classList.contains('selected')) {
-        day_item_el.classList.remove('selected');
-    } else {
-        day_item_el.classList.add('selected');
-    }
-}, true);
+    days_list_el.addEventListener('click', function(event) {
+        day_item_el = event.target;
+        if (day_item_el.classList.contains('selected')) {
+            day_item_el.classList.remove('selected');
+        } else {
+            day_item_el.classList.add('selected');
+        }
+    }, true);
+}
